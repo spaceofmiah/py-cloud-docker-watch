6f0b7c06341f9018323fa8154820fee081593d70
spaceofmiah <agbonzeosazuwa@gmail.com>
2024-02-01 13:32:59 +0100

    load docker client and fix image pulling if not found

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from tap import Tap
+import logging
+import docker
 
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class ScriptArguments(Tap):
@@ -12,14 +18,34 @@ class ScriptArguments(Tap):
     aws_region: str  # AWS region
 
 
-
 args = ScriptArguments(underscores_to_dashes=True).parse_args()
 
-print(
-"Docker Image: ", args.docker_image, "\nAWS CWG: ", 
-args.aws_cloudwatch_group, "\nAWS CWS: ", 
-args.aws_cloudwatch_stream, "\nAWS AKID: ", 
-args.aws_access_key_id, "\nAWS SAK: ", 
-args.aws_secret_access_key, "\nBash Script: \n\n", 
-args.bash_command, "\n\n",
-)
+if __name__ == "__main__":
+    try:
+        docker_client = docker.from_env()
+    except Exception as e:
+        print("\n\n[ExecutionFailed]: Docker is not running on your machine. Start docker desktop and retry\n")
+        exit(1)
+    
+    # check if the docker image exists
+    print(f"Locating image {args.docker_image}...")
+    image_exists = len(docker_client.images.list(name=args.docker_image)) > 0
+    if image_exists is False:
+        print(f"Image {args.docker_image} not found\nKindly ensure you're connected to the internet\npulling...")
+        docker_client.images.pull(args.docker_image)
+        print(f"Image {args.docker_image} pulled successfully")
+    
+    print(f"\Starting docker container from image {args.docker_image}")
+    container = docker_client.containers.run(args.docker_image, args.bash_command)
+    print(f"Docker container started with id {container.id}\n Container logs:\n\n")
+    for line in container.logs(stream=True):
+        print(line.strip())
+
+    # print(
+    # "Docker Image: ", args.docker_image, "\nAWS CWG: ", 
+    # args.aws_cloudwatch_group, "\nAWS CWS: ", 
+    # args.aws_cloudwatch_stream, "\nAWS AKID: ", 
+    # args.aws_access_key_id, "\nAWS SAK: ", 
+    # args.aws_secret_access_key, "\nBash Script: \n\n", 
+    # args.bash_command, "\n\n",
+    # )
